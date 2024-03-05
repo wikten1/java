10f4e8869a4eadb698a66ba966268c7c6de0565f
Wikten Alves <wiktenalves@gmail.com>
2024-03-05 11:08:01 -0300

    Aula 12 e 13 feitas

--- src/Aula02TiposPrimitivos.java
+++ src/Aula02TiposPrimitivos.java
@@ -1,16 +1,19 @@
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         //int, duble, float, char, byte, short, long, boolean
-        int idade = 10;
-        double salarioDouble = 2000;
-        float salarioFloat = 2500;
-        byte idadeByte = 10;
-        short idadeShort = 10;
+        int idade = (int) 100000000000000L;
+        long numeroGrande = (long) 155.23;
+        double salarioDouble = 2000.0D;
+        float salarioFloat = (float) 2500.0D;
+        byte idadeByte = 127;
+        short idadeShort = 3200;
         boolean verdadeiro = true;
         boolean falso = false;
         char caractere = 'A';
+        String frase = "Esse é um grande texto!";
         System.out.println("A idade é: "+idade+" anos");
         System.out.println(verdadeiro);
         System.out.println(caractere);
+        System.out.println(frase);
     }
 }
