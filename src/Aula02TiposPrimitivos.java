78c2a2029f0d5792230cf7d9608bcd6874fa1090
Wikten Alves <wiktenalves@gmail.com>
2024-03-05 00:23:08 -0300

    Aulas de 1 a 12 feitas

---
+++ src/Aula02TiposPrimitivos.java
@@ -0,0 +1,16 @@
+public class Aula02TiposPrimitivos {
+    public static void main(String[] args) {
+        //int, duble, float, char, byte, short, long, boolean
+        int idade = 10;
+        double salarioDouble = 2000;
+        float salarioFloat = 2500;
+        byte idadeByte = 10;
+        short idadeShort = 10;
+        boolean verdadeiro = true;
+        boolean falso = false;
+        char caractere = 'A';
+        System.out.println("A idade é: "+idade+" anos");
+        System.out.println(verdadeiro);
+        System.out.println(caractere);
+    }
+}
---
+++ src/academy/devdojo/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,10 @@
+package academy.devdojo.maratonajava.introducao;
+
+/**
+ * Isso é um comentário do java doc
+ */
+public class Aula01OlaDevDojo {
+    public static void main(String[] args){
+        System.out.println("Olá mundo!");
+    }
+}
