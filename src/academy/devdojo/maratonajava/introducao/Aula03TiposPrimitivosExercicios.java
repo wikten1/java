c40aeb5fb44fc2a253a87e93b912dfc16994aa98
Wikten Alves <wiktenalves@gmail.com>
2024-03-07 10:45:21 -0300

    Aula 02 e desafio feitos
    Aula 02 de tipos primitivos e desafio feito.

---
+++ src/academy/devdojo/maratonajava/introducao/Aula03TiposPrimitivosExercicios.java
@@ -0,0 +1,12 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula03TiposPrimitivosExercicios {
+    public static void main(String[] args){
+        String nome = "Wikten";
+        String endereco = "Rua C, Lote 7, QD.3, CS.5";
+        float salario = 3000f;
+        String data = "06/03/2024";
+
+        System.out.println("Eu "+nome+" morando no endereço "+endereco+" Confirmo que recebi o salário de "+salario+" na data "+data);
+        
+}
