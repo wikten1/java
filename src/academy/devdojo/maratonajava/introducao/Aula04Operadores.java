cfe6d97f3738263e663b91974c7fb46fba805f24
Wikten Alves <wiktenalves@gmail.com>
2024-03-07 12:39:32 -0300

    Aula 1 a 4 de operadores feitas

---
+++ src/academy/devdojo/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,41 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        //+ - * /
+        int numero1 = 10;
+        int numero2 = 20;
+        double resultado = numero1/(double)numero2;
+        System.out.println(numero2+numero1);
+        System.out.println(resultado);
+
+        //%
+        int resto = 21 % 7;
+        System.out.println(resto);
+
+        // < > <= >= == !=
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 10;
+        boolean isDezDiferenteDeVinte = 10 != 10;
+        System.out.println("isDezMaiorQueVinte " +isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte " +isDezMenorQueVinte);
+        System.out.println("isDezIgualVinte " +isDezIgualVinte);
+        System.out.println("isDezDiferenteDeVinte " +isDezDiferenteDeVinte);
+
+        //&& (AND) || (OR) ! (NOT)
+        int idade = 29;
+        float salario = 3500f;
+        boolean isDentroDaLeiMaiorQueTrinta = idade >= 30 && salario >= 4612;
+        boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381;
+        System.out.println("isDentroDaLeiMaiorQueTrinta "+isDentroDaLeiMaiorQueTrinta);
+        System.out.println("isDentroDaLeiMenorQueTrinta "+isDentroDaLeiMenorQueTrinta);
+
+        double valorTotalContaCorrente = 200;
+        double valorTotalContaPoupanca = 10000;
+        float valorPlayStation = 500f;
+        boolean isPlaystationCincoCompravel = valorTotalContaCorrente > valorPlayStation || valorTotalContaPoupanca > valorPlayStation;
+        System.out.println("isPlaystationsCincoCompravel "+isPlaystationCincoCompravel);
+
+    }
+}
