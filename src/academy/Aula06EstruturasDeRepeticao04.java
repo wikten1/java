301286b358105df4afabe56a3b52bc7a160b44e7
Wikten Alves <wiktenalves@gmail.com>
2024-03-16 19:21:53 -0300

    Aulas estruturas de repetição 1 a 3

---
+++ src/academy/Aula06EstruturasDeRepeticao04.java
@@ -0,0 +1,17 @@
+package academy;
+
+public class Aula06EstruturasDeRepeticao04 {
+    //dado um valor de um carro descubra em quantas vezes ele pode ser parcelado.
+    //condição valor da parcela >= 1000.
+    public static void main(String[] args) {
+        float valorCarro = 50000f;
+        System.out.println("É possível parcelar o carro em: ");
+        for(int parcela = 1; parcela <= valorCarro; parcela++){
+            float resultado = valorCarro/parcela;
+            if (resultado < 1000) {
+                break;
+            }
+            System.out.println("Parcela "+parcela+" R$ "+resultado);
+        }
+    }
+}
---
+++ src/academy/devdojo/maratonajava/introducao/Aula05EstruturasCondicionais06.java
@@ -0,0 +1,26 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais06 {
+    public static void main(String[] args) {
+        byte dia = 1;
+
+        switch (dia) {
+            case 1:
+            case 7:
+                System.out.println("Fim de semana");
+                break;
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                System.out.println("Dia útil");
+                break;
+
+            default:
+                System.out.println("Valor inválido!");
+                break;
+        }
+
+    }
+}
