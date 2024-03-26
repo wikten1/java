64fa01e53315f2471c9bc438631f653df3ded249
Wikten Alves <wiktenalves@gmail.com>
2024-03-26 01:16:59 -0300

    Aula7arreys PT 1 Feito

--- src/academy/devdojo/maratonajava/introducao/Aula07Arreys01.java
+++ src/academy/devdojo/maratonajava/introducao/Aula07Arreys01.java
@@ -1,5 +1,15 @@
 package academy.devdojo.maratonajava.introducao;
 
 public class Aula07Arreys01 {
-    int[] idades = null;
+    public static void main(String[] args) {
+        //Essa variavel a baixo é do tipo reference igual a String
+        //Um arrey é considerado um objeto na memória
+        int[] idades = new int[3];
+        idades [0] = 21;
+        idades [1] = 15;
+        idades [2] = 11;
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
 }
